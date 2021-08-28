fcce0408079f075a6d70f83995046955e3d81d75
unknown <zachariah11jerobert@gmail.com>
2021-08-28 23:16:44 +0530

    router setuped

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
-const express=require('express');
-const app=express();
+const express = require("express");
+const app = express();
 
-app.listen(3001,()=>{
-    console.log('sever is running on port 3001');
-})
+const db = require("./models");
+
+// Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("sever is running on port 3001");
+  });
+});
