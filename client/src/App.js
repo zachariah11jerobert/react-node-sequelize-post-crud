feaf326d80c9bd547867b9b5186849e70c89d56a
unknown <zachariah11jerobert@gmail.com>
2021-08-29 16:07:04 +0530

    react part : crated ui with css to list post

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
+  const [listOfPosts, setListOfPosts] = useState([]);
+
   useEffect(() => {
     axios.get("http://localhost:3001/posts").then((response) => {
-      console.log(response.data);
+      setListOfPosts(response.data);
     });
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {listOfPosts.map((value, key) => {
+        return (
+          <div className="post" key={key}>
+            <div className="title">{value.title}</div>
+            <div className="body">{value.postText}</div>
+            <div className="footer">{value.username}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
