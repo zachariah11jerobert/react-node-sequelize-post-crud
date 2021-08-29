7306b15ba8f9a62efa13ca87c08753995236a032
unknown <zachariah11jerobert@gmail.com>
2021-08-29 15:44:37 +0530

    react part : data retrived from server without cors

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      console.log(response.data);
+    });
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
